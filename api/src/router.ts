543f6e53c7f47457151ad294482dcce64b03d7cd
Wallace Vieira <wallaceofc@hotmail.com>
2023-12-05 19:34:03 -0300

    feat: list and create routes

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
+
 import { listCategories } from './app/useCases/categories/listCategories';
+import { createCategorie } from './app/useCases/categories/createCategory';
 
 export const router = Router();
 
 router.get('/categories', listCategories);
 
-router.post('/categories', (req, res) => {
-  res.send('OK');
-});
+router.post('/categories', createCategorie);
 
 router.get('/categories/:categoryId/products', (req, res) => {
   res.send('OK');
