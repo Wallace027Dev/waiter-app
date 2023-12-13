d1b7600b301851adf9f46d7f8dc7e4296eef53c3
Wallace Vieira <wallaceofc@hotmail.com>
2023-12-13 15:48:08 -0300

    feat: Categories component

--- app/src/Main/styles.ts
+++ app/src/Main/styles.ts
@@ -5,23 +5,22 @@ const isAndroid = Platform.OS === 'android';
 
 export const Container = styled.SafeAreaView`
 	margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : '0'};
+	background:	#fafafa;
 	flex: 1;
 `;
 
 export const CategoriesContainer = styled.View`
 	height: 73px;
-	background: red;
 	margin-top: 34px;
 `;
 
 export const MenuContainer = styled.View`
-	background: blue;
 	flex: 1;
 `;
 
 export const Footer = styled.View`
 	min-height: 110px;
-	background: green;
+	background: #fff;
 `;
 
 export const FooterContiner = styled.SafeAreaView``;
