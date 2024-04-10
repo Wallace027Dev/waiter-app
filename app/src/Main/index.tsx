bfe02916f20a9e428ac19b7be9c73a256a247ec8
wallacevieira <wallaceofc@hotmail.com>
2024-04-10 08:22:52 -0300

    feat: cartItems

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -1,15 +1,31 @@
 import { useState } from 'react';
+
 import { Button } from '../components/Button';
 import { Categories } from '../components/Categories';
 import { Header } from '../components/Header';
 import { Menu } from '../components/Menu';
 import { TableModal } from '../components/TableModal';
+import { Cart } from '../components/Cart';
 
 import { Container, CategoriesContainer, MenuContainer, Footer, FooterContiner } from './styles';
+
+import { products } from '../mocks/products';
+
+import { CartItem } from '../types/CartItem';
 
 export function Main() {
 	const [isTableModalVisible, setIsTableModalVisible] = useState(false);
 	const [selectedTable, setSelectTable] = useState('');
+	const [cartItems, setCartItems] = useState<CartItem[]>([
+		{
+			quantity: 1,
+			product: products[0],
+		},
+		{
+			quantity: 2,
+			product: products[1],
+		},
+	]);
 
 	function handleSaveTable(table: string) {
 		setSelectTable(table);
@@ -43,6 +59,10 @@ export function Main() {
 						<Button onPress={() => setIsTableModalVisible(true)}>
 						Novo Pedido
 						</Button>
+					)}
+
+					{selectedTable && (
+						<Cart cartItems={cartItems} />
 					)}
 				</FooterContiner>
 
